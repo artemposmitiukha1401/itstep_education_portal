33c1ea09b616f1afbe07d5f7a767dbfefccfb206
artem <artemposmitiukha@gmail.com>
2026-04-08 09:06:00 +0300

    topic page

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,8 +1,10 @@
 import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import SubjectMap from "../SubjectNetwork/SubjectMap";
+import TopicPage from "../TopicPage/TopicPage";
 import SubjectPage from "../SubjectPage/SubjectPage";
 import "./Root.css";
 import Header from "../Header/Header";
+import NotesPage from "../NotesPage/NotesPage";
 
 function Root() {
   return (
@@ -13,6 +15,10 @@ function Root() {
         <Route path="/home" element={<SubjectMap />} />
         <Route path="/about" element={<div>About Page</div>} />
         <Route path="/subject/:id" element={<SubjectPage />} />
+          <Route path="/subject/:subjectId" element={<SubjectPage />} />
+  <Route path="/subject/:subjectId/topic/:topicId" element={<TopicPage/>} /> 
+  <Route path="/subject/:subjectId/topic/:topicId/notes" element={<NotesPage/>} />
+
       </Routes>
       <Outlet />
     </div>
